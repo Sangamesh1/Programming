3bd05cc9de0cb857d0be1c9684b7378240a33132
Sangamesh1 <daphalapurs@gmail.com>
2017-03-04 15:20:20 +0530

    more java programs
    mostly on exception handling

---
+++ New Folder/Talikoti.java
@@ -0,0 +1,11 @@
+run:
+The station to reach for talikoti: Athani and Bijapur 
+Do you want to travel today? yes:1 No:0 
+0
+The Platform No. to alight for Talikoti: 5
+The ticket price to reach talikoti from bijapur is : 200
+The Platform No. to alight for Talikoti: 2
+The ticket price to reach bijapur from athani is : 40
+TicketPrice at Bijapur Station: -56
+TicketPrice at athani Station: 40
+BUILD SUCCESSFUL (total time: 2 seconds)
